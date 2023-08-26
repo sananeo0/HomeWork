26d1aea7a6127ae5142b8bb8fae3543b703776a9
sananeo0 <sananeo@code.edu.az>
2023-08-26 23:19:19 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace HomeWork
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 1;           
+            string input = "CodeAcademy is a good place to study";
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i]==' ')
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
